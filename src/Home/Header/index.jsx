34d57e9c2c4a06bc2f2c4726afdd5abf0c9a0b91
Thanh Minh Ho Dac <hodacthanhminh@gmail.com>
2021-11-11 12:06:00 +0700

    adding style header

--- src/Home/Header/index.jsx
+++ src/Home/Header/index.jsx
@@ -1,9 +1,18 @@
 // libs
 import React from 'react';
 
+// components
+import Logo from './Logo';
+
 // styles
 import './style.scss';
 
-const Header = () => <div className="header-wrapper">Header</div>;
+const Header = () => (
+  <div className="header-wrapper">
+    <div className="header-inner">
+      <Logo url="https://www.nhaccuatui.com/" />
+    </div>
+  </div>
+);
 
 export default Header;
