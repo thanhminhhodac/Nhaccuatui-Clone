4033d6eb338e0353733134a6dedacb8ff1860bc2
Thanh Minh Ho Dac <hodacthanhminh@gmail.com>
2021-11-11 13:54:09 +0700

    edit header logic

--- src/Home/Header/index.jsx
+++ src/Home/Header/index.jsx
@@ -15,16 +15,19 @@ import MoreImage from '../../assets/ic_more_new.png';
 const Header = () => (
   <div className="header-wrapper">
     <div className="header-inner">
-      <Logo url="https://www.nhaccuatui.com/" />
-      <NewVersion url="https://beta.nhaccuatui.com/" />
-      <Menu title="Bài Hát" />
-      <Menu title="Playlist" />
-      <Menu title="Tuyển Tập" />
-      <Menu title="Video" />
-      <Menu title="BXH" />
-      <Menu title="Chủ Đề" />
-      <Menu title="Top 100" />
-      <Menu title="Khác" imageUrl={MoreImage} />
+      <div className="header-left">
+        <Logo url="https://www.nhaccuatui.com/" />
+        <NewVersion url="https://beta.nhaccuatui.com/" />
+        <Menu title="Bài Hát" />
+        <Menu title="Playlist" />
+        <Menu title="Tuyển Tập" />
+        <Menu title="Video" />
+        <Menu title="BXH" />
+        <Menu title="Chủ Đề" />
+        <Menu title="Top 100" />
+        <Menu title="Khác" imageUrl={MoreImage} />
+      </div>
+      <div className="header-right">Left side</div>
     </div>
   </div>
 );
