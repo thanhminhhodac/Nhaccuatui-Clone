bb2f0590d53bd959fe5e520fb8c4f4d5463c6220
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-11-12 08:00:26 +0700

    init Playlist-suggestion

--- src/Home/PlaylistSuggestion/index.jsx
+++ src/Home/PlaylistSuggestion/index.jsx
@@ -1,11 +1,18 @@
 // lib
 import React from 'react';
 
+// components
+import Heading from './Heading';
+import PlaylistCard from './PlaylistCard';
+
 // style
 import './style.scss';
 
 const PlaylistSuggestion = () => (
-  <div className="playlist-suggestion-wrapper">Playlist Suggestion</div>
+  <div className="playlist-suggestion-wrapper">
+    <Heading />
+    <PlaylistCard />
+  </div>
 );
 
 export default PlaylistSuggestion;
