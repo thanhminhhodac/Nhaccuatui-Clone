11db0b047039ad9c40a20fbb253730aedad3c339
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-11-12 15:29:37 +0700

    edit menu components

--- src/Home/Header/Menu/index.jsx
+++ src/Home/Header/Menu/index.jsx
@@ -1,29 +1,28 @@
 // libs
 import React from 'react';
 import PropTypes from 'prop-types';
-
+// components
+import MenuItem from './mains/MenuItem';
 // style
 import './style.scss';
 
-const Menu = ({ title, imageUrl }) => (
-  <div className="menu-item">
-    <div className="menu-item-title">
-      {imageUrl ? (
-        <img src={imageUrl} alt={title} className="menu-item-image" />
-      ) : (
-        <span>{title}</span>
-      )}
+const Menu = ({ menuList }) => (
+  <div className="menu-wrapper">
+    <div className="menu-wrapper-inner">
+      {menuList.map((menu) => (
+        <MenuItem imageUrl={menu.imageUrl} title={menu.title} />
+      ))}
     </div>
   </div>
 );
 
 Menu.propTypes = {
-  title: PropTypes.string.isRequired,
-  imageUrl: PropTypes.string,
-};
-
-Menu.defaultProps = {
-  imageUrl: null,
+  menuList: PropTypes.arrayOf(
+    PropTypes.shape({
+      imageUrl: PropTypes.string,
+      title: PropTypes.string.isRequired,
+    }),
+  ).isRequired,
 };
 
 export default Menu;
