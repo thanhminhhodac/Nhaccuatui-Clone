7dc9cd0711a1c63e69d17032fb552c76812a60a5
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-11-12 14:06:58 +0700

    fix Authenthication components className

--- src/Home/Header/Authentication/index.jsx
+++ src/Home/Header/Authentication/index.jsx
@@ -6,8 +6,8 @@ import React from 'react';
 import './style.scss';
 
 const Authentication = () => (
-  <div className="authentication">
-    <div className="authentication-inner">
+  <div className="authentication-wrapper">
+    <div className="authentication-wrapper-inner">
       <div className="authentication-item btn-signin">Đăng nhập</div>
       <div className="authentication-item btn-signup">Đăng ký</div>
     </div>
