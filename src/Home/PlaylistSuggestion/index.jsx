b7bc91f0ad297039f65899e663938298649a3ad8
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-11-12 17:40:24 +0700

    Split components from PlaylistSuggestion

--- src/Home/PlaylistSuggestion/index.jsx
+++ src/Home/PlaylistSuggestion/index.jsx
@@ -3,37 +3,24 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 // components
-import Heading from './Heading';
-import PlaylistCard from './PlaylistCard';
+import Heading from '../components/Heading';
 
 // style
 import './style.scss';
+import PlaylistSuggestionBody from './PlaylistSuggestionBody';
 
 const PlaylistSuggestion = ({ title, suggestList }) => (
   <div className="playlist-suggestion-wrapper">
-    <Heading title={title} />
-    <div className="playlist-suggestion-body">
-      {
-        // eslint-disable-next-line arrow-parens
-        suggestList.map(suggest => (
-          <PlaylistCard
-            imageUrl={suggest.imageUrl}
-            suggestion={suggest.suggestion}
-          />
-        ))
-      }
+    <div className="playlist-suggestion-wrapper-inner">
+      <Heading title={title} />
+      <PlaylistSuggestionBody suggestList={suggestList} />
     </div>
   </div>
 );
 
 PlaylistSuggestion.propTypes = {
   title: PropTypes.string.isRequired,
-  suggestList: PropTypes.arrayOf(
-    PropTypes.shape({
-      imageUrl: PropTypes.string.isRequired,
-      suggestion: PropTypes.string.isRequired,
-    }),
-  ).isRequired,
+  suggestList: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
 export default PlaylistSuggestion;
