c723cabf719931ae7ee757801d1f8da253e719b9
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-11-12 15:33:33 +0700

    Datasource && Header component edit

---
+++ src/Datasource/Home/Header/Menu/index.js
@@ -0,0 +1,29 @@
+import MoreImage from '../../../../assets/ic_more_new.png';
+
+export const menuList = [
+  {
+    title: 'Bài Hát',
+  },
+  {
+    title: 'Playlist',
+  },
+  {
+    title: 'Tuyển tập',
+  },
+  {
+    title: 'Video',
+  },
+  {
+    title: 'BXH',
+  },
+  {
+    title: 'Chủ đề',
+  },
+  {
+    title: 'Top 100',
+  },
+  {
+    title: 'Khác',
+    imageUrl: MoreImage,
+  },
+];
--- src/Home/Header/index.jsx
+++ src/Home/Header/index.jsx
@@ -13,7 +13,7 @@ import Search from './Search';
 import './style.scss';
 
 // other
-import MoreImage from '../../assets/ic_more_new.png';
+import { menuList } from '../../Datasource/Home/Header/Menu';
 import UploadeImage from '../../assets/ic_upload.png';
 import StoreImage from '../../assets/ic_store.png';
 
@@ -23,14 +23,7 @@ const Header = () => (
       <div className="header-left">
         <Logo url="https://www.nhaccuatui.com/" />
         <NewVersion url="https://beta.nhaccuatui.com/" />
-        <Menu title="Bài Hát" />
-        <Menu title="Playlist" />
-        <Menu title="Tuyển Tập" />
-        <Menu title="Video" />
-        <Menu title="BXH" />
-        <Menu title="Chủ Đề" />
-        <Menu title="Top 100" />
-        <Menu title="Khác" imageUrl={MoreImage} />
+        <Menu menuList={menuList} />
       </div>
       <div className="header-right">
         <Search />
