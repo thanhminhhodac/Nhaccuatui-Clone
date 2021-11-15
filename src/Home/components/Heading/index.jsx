031e8c4ce327141e4ca012243e9b4db173ad331b
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-11-15 23:31:02 +0700

    Edit Heading components

--- src/Home/components/Heading/index.jsx
+++ src/Home/components/Heading/index.jsx
@@ -1,17 +1,25 @@
 // libs
 import React from 'react';
 import PropTypes from 'prop-types';
-
 // style
 import './style.scss';
+// components
+import { RightOutlined } from '@ant-design/icons';
 
-const Heading = ({ title }) => (
+const Heading = ({ title, icon }) => (
   <div className="heading">
     <span className="heading-title">{title}</span>
+    {icon && <RightOutlined className="right-outlined" />}
   </div>
 );
 
-Heading.propTypes = { title: PropTypes.string };
-Heading.defaultProps = { title: 'Playlist Suggestions' };
+Heading.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.bool,
+};
+
+Heading.defaultProps = {
+  icon: false,
+};
 
 export default Heading;
