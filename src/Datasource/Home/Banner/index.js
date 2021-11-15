bbf79bf6183166a632b380682f77230a51e55b1e
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-11-15 10:09:37 +0700

    create datasource

---
+++ src/Datasource/Home/Banner/index.js
@@ -0,0 +1,10 @@
+import bannerImage from '../../../assets/Banner/bg_web.png';
+
+export const bannerData = {
+  bannerBackground: bannerImage,
+  bannerTitle: 'Gợi ý dành cho bạn',
+  bannerContent: 'Thưởng thức nhưng ca khú phù hợp nhất với bạn',
+  bannerButton: {
+    label: 'Nghe bài hát',
+  },
+};
