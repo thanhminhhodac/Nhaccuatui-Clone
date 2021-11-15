01053a20423dfe5ed0d279dc24450293f283afe7
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-11-15 23:13:07 +0700

    Card & CardImage

--- src/Home/components/Card/components/CardImage/index.jsx
+++ src/Home/components/Card/components/CardImage/index.jsx
@@ -1,16 +1,22 @@
 // lib
 import React from 'react';
 import PropTypes from 'prop-types';
+// style
+import './style.scss';
 
-const CardImage = ({ cardImage, description }) => (
+const CardImage = ({ cardImage, cardDescription }) => (
   <div className="card-image-wrapper">
-    <img className="card-image" src={cardImage} alt={description} />
+    <img
+      className="card-image-wrapper-inner"
+      src={cardImage}
+      alt={cardDescription}
+    />
   </div>
 );
 
 CardImage.propTypes = {
   cardImage: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
+  cardDescription: PropTypes.string.isRequired,
 };
 
 export default CardImage;
--- src/Home/components/Card/index.jsx
+++ src/Home/components/Card/index.jsx
@@ -3,20 +3,29 @@ import React from 'react';
 import PropTypes from 'prop-types';
 // components
 import CardAvatar from './mains/CardAvatar';
+import CardContent from './mains/CardContent';
 // style
 import './style.scss';
 
-const Card = ({ cardImage, description }) => (
+const Card = ({ cardItem, cardType }) => (
   <div className="card-wrapper">
-    <div className="card-wrapper-inner">
-      <CardAvatar cardImage={cardImage} description={description} />
-    </div>
+    <CardAvatar cardAvatar={cardItem.avatar} cardType={cardType} />
+    {cardItem.content && (
+      <CardContent cardContent={cardItem.content} cardType={cardType} />
+    )}
   </div>
 );
 
 Card.propTypes = {
-  cardImage: PropTypes.string.isRequired,
-  description: PropTypes.string.isRequired,
+  cardItem: PropTypes.shape({
+    avatar: PropTypes.shape({}).isRequired,
+    content: PropTypes.shape({}),
+  }).isRequired,
+  cardType: PropTypes.string,
+};
+
+Card.defaultProps = {
+  cardType: null,
 };
 
 export default Card;
