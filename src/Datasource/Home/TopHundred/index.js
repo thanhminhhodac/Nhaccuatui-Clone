576ade844f9ec98928f2a97f04ac58929318bc1d
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-11-15 14:43:01 +0700

    Create Datasource

---
+++ src/Datasource/Home/TopHundred/index.js
@@ -0,0 +1,28 @@
+import tHImage1 from '../../../assets/TopHundred/1.jpg';
+import tHImage2 from '../../../assets/TopHundred/2.jpg';
+import tHImage3 from '../../../assets/TopHundred/3.jpg';
+import tHImage4 from '../../../assets/TopHundred/4.jpg';
+import tHImage5 from '../../../assets/TopHundred/5.jpg';
+
+export const topHundredList = [
+  {
+    topicImage: tHImage1,
+    description: 'Nhạc trẻ',
+  },
+  {
+    topicImage: tHImage2,
+    description: 'Pop USUK',
+  },
+  {
+    topicImage: tHImage3,
+    description: 'Rap Việt',
+  },
+  {
+    topicImage: tHImage4,
+    description: 'Blue Jazz',
+  },
+  {
+    topicImage: tHImage5,
+    description: 'Country',
+  },
+];
