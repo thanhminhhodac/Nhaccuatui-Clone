6578b4baeeb862c51424663a47057b1d1bc0e896
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-11-17 10:29:07 +0700

    create dataSources NewPublished

---
+++ src/dataSources/Home/NewPublished/index.js
@@ -0,0 +1,143 @@
+import nPI1 from '../../../assets/NewPublished/1.jpg';
+import nPI2 from '../../../assets/NewPublished/2.jpg';
+import nPI3 from '../../../assets/NewPublished/3.jpg';
+import nPI4 from '../../../assets/NewPublished/4.jpg';
+import nPI5 from '../../../assets/NewPublished/5.jpg';
+import nPI6 from '../../../assets/NewPublished/6.jpg';
+import nPI7 from '../../../assets/NewPublished/7.jpg';
+import nPI8 from '../../../assets/NewPublished/8.jpg';
+import nPI9 from '../../../assets/NewPublished/9.jpg';
+import nPI10 from '../../../assets/NewPublished/10.jpg';
+
+export const newPublishedList = [
+  {
+    id: 'nPI1',
+    avatar: {
+      avatarImage: nPI1,
+      avatarDescription: 'chiếc hộp',
+    },
+    content: {
+      title: 'Chiếc Hộp',
+      artist: ['Bích Phương'],
+    },
+    count: {
+      countType: 'Listen',
+      countAmount: 17609,
+    },
+  },
+  {
+    id: 'nPI2',
+    avatar: {
+      avatarImage: nPI2,
+      avatarDescription: 'MoonBow',
+    },
+    content: {
+      title: 'MoonBow',
+      artist: ['TIA', 'Thái Vũ (BlackBi)', 'CM1X'],
+    },
+    count: {
+      countType: 'Listen',
+      countAmount: 1652,
+    },
+  },
+  {
+    id: 'nPI3',
+    avatar: {
+      avatarImage: nPI3,
+      avatarDescription: 'Thật Ngoan!',
+    },
+    content: {
+      title: 'Thật Ngoan!',
+      artist: ['2T', 'JD'],
+    },
+    count: {
+      countType: 'Listen',
+      countAmount: 16,
+    },
+  },
+  {
+    id: 'nPI4',
+    avatar: {
+      avatarImage: nPI4,
+      avatarDescription: 'Chẳng Còn Rung Động',
+    },
+    content: {
+      title: 'Chẳng Còn Rung Động',
+      artist: ['Tường Quân'],
+    },
+    count: {
+      countType: 'Listen',
+      countAmount: 33,
+    },
+  },
+  {
+    id: 'nPI5',
+    avatar: {
+      avatarImage: nPI5,
+      avatarDescription: 'Duyên Tại Ý Trời',
+    },
+    content: {
+      title: 'Duyên Tại Ý Trời',
+      artist: ['Bằng Cường'],
+    },
+    count: {
+      countType: 'Listen',
+      countAmount: 45702,
+    },
+  },
+  {
+    id: 'nPI6',
+    avatar: {
+      avatarImage: nPI6,
+      avatarDescription: 'XOXO',
+    },
+    content: {
+      title: 'XOXO',
+      artist: ['JEON SOMI'],
+    },
+  },
+  {
+    id: 'nPI7',
+    avatar: {
+      avatarImage: nPI7,
+      avatarDescription: 'Music Of The Spheres',
+    },
+    content: {
+      title: 'Music Of The Spheres',
+      artist: ['Coldplay'],
+    },
+  },
+  {
+    id: 'nPI8',
+    avatar: {
+      avatarImage: nPI8,
+      avatarDescription: 'Imagine Dragons EP',
+    },
+    content: {
+      title: 'Imagine Dragons EP',
+      artist: ['Imagine Dragons'],
+    },
+  },
+  {
+    id: 'nPI9',
+    avatar: {
+      avatarImage: nPI9,
+      avatarDescription: 'Love For Sale (Deluxe)',
+    },
+    content: {
+      title: 'Love For Sale (Deluxe)',
+      artist: ['Tony Bennett', 'Lady Gaga'],
+    },
+  },
+  {
+    id: 'nPI10',
+    avatar: {
+      avatarImage: nPI10,
+      avatarDescription: "It's All Make Sense In The End",
+    },
+    content: {
+      title: "It's All Make Sense In The End",
+      artist: ['James Arthur'],
+    },
+  },
+];
