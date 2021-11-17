e869b9077a9e5993cd5a72cf8ffc7d3abebcee3b
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-11-17 14:27:23 +0700

    Card Update style CardImage && cardAvatar components

--- src/Home/components/Card/mains/CardAvatar/index.jsx
+++ src/Home/components/Card/mains/CardAvatar/index.jsx
@@ -6,11 +6,14 @@ import CardImage from '../../components/CardImage';
 // style
 import './style.scss';
 
+const typeCardPlay = ['playlist-suggestion', 'new-published'];
+
 const CardAvatar = ({ cardAvatar, cardType }) => (
   <div className={['card-avatar-wrapper', `card-${cardType}`].join(' ')}>
     <CardImage
       cardImage={cardAvatar.avatarImage}
       cardDescription={cardAvatar.avatarDescription}
+      cardPlay={typeCardPlay.includes(cardType)}
     />
   </div>
 );
