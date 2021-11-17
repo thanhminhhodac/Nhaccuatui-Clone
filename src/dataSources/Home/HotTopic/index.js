07b3d16b2fa2239533d9079fa2a2fedc2c00b3ba
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-11-17 08:57:18 +0700

    Add key for map in HotTopic

--- src/dataSources/Home/HotTopic/index.js
+++ src/dataSources/Home/HotTopic/index.js
@@ -6,22 +6,27 @@ import hTPI5 from '../../../assets/HotTopic/5.jpg';
 
 export const hotTopicList = [
   {
+    id: 'hTP1',
     topicImage: hTPI1,
     description: 'gaming',
   },
   {
+    id: 'hTP2',
     topicImage: hTPI2,
     description: 'top hits 2021',
   },
   {
+    id: 'hTP3',
     topicImage: hTPI3,
     description: 'EDM',
   },
   {
+    id: 'hTP4',
     topicImage: hTPI4,
     description: 'epic music',
   },
   {
+    id: 'hTP5',
     topicImage: hTPI5,
     description: 'rap việt',
   },
