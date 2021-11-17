2bf331dcf5392a6af14f1cbc0bf3dd8d26a0ee1f
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-11-17 10:35:23 +0700

    create NewPublished Component

---
+++ src/Home/NewPublished/mains/NewPublishedBody/index.jsx
@@ -0,0 +1,25 @@
+// libs
+import React from 'react';
+import PropTypes from 'prop-types';
+// components
+import Card from '../../../components/Card';
+// style
+import './style.scss';
+
+const NewPublishedBody = ({ publishedList }) => (
+  <div className="new-published-body">
+    {publishedList.map((publishedItem) => (
+      <Card
+        key={publishedList.id}
+        cardItem={publishedItem}
+        cardType="new-published"
+      />
+    ))}
+  </div>
+);
+
+NewPublishedBody.propTypes = {
+  publishedList: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+};
+
+export default NewPublishedBody;
