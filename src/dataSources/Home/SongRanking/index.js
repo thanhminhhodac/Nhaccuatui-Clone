4b902363faec588f0d57e3bc1e67c5840d27dbb4
Thanh Minh Ho Dac <hodacthanhminh@gmail.com>
2021-11-18 15:04:24 +0700

    Create dataSources

---
+++ src/dataSources/Home/SongRanking/index.js
@@ -0,0 +1,476 @@
+export const songRankingList = [
+  {
+    sRP: 'sRP1',
+    topList: [
+      {
+        id: 'sId1',
+        avatar: {
+          avatarImage: 'ádf',
+          avatarDescription: 'da',
+        },
+        content: {
+          title: 'Ái nộ',
+          artist: [
+            {
+              aPId: 'aP26',
+              artistName: 'Masew',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId2',
+        content: {
+          title: 'Câu Hứa Chưa Vẹn Tròn',
+          artistName: [
+            {
+              aPId: 'aP27',
+              artistName: 'Phát',
+            },
+            {
+              aPId: 'aP28',
+              artistName: 'Huy T4',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId3',
+        content: {
+          title: 'Cưới Thôi',
+          artistName: [
+            {
+              aPId: 'aP26',
+              artistName: 'Masew',
+            },
+            {
+              aPId: 'aP29',
+              artistName: 'B Ray',
+            },
+            {
+              aPId: 'aP30',
+              artistName: 'TAP',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId4',
+        content: {
+          title: 'Thê Lương',
+          artistName: [
+            {
+              aPId: 'aP31',
+              artistName: 'Phúc Chinh',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId5',
+        content: {
+          title: 'Tình Thương Phu Thê',
+          artistName: [
+            {
+              aPId: 'aP32',
+              artistName: 'Chí Hướng',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId6',
+        content: {
+          title: 'Diệu dàng em đến',
+          artistName: [
+            {
+              aPId: 'aP33',
+              artistName: 'ERIK',
+            },
+            {
+              aPId: 'aP34',
+              artistName: 'NinjaZ',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId7',
+        content: {
+          title: 'Sài Gòn đau lòng quá',
+          artist: [
+            {
+              aPId: 'aP35',
+              artistName: 'Đinh Dũng',
+            },
+            {
+              aPId: 'aP36',
+              artistName: 'ACV',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId8',
+        content: {
+          title: 'Câu Hẹn Câu Thề',
+          artist: [
+            {
+              aPId: 'aP20',
+              artistName: 'Hứa Kim Tuyền',
+            },
+            {
+              aPId: 'aP21',
+              artistName: 'Hoàng Duyên',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId9',
+        content: {
+          title: 'Có Hẹn Với Thanh Xuân',
+          artist: [
+            {
+              aPId: 'aP37',
+              artistName: 'MONSTAR',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId10',
+        content: {
+          title: 'Muộn Rồi Mà Sao Còn',
+          artist: [
+            {
+              aPId: 'aP38',
+              artistName: 'Sơn Tùng M-TP',
+            },
+          ],
+        },
+      },
+    ],
+  },
+  {
+    sRP: 'sRP2',
+    topList: [
+      {
+        id: 'sId1',
+        avatar: {
+          avatarImage: 'ádf',
+          avatarDescription: 'da',
+        },
+        content: {
+          title: 'Ái nộ',
+          artist: [
+            {
+              aPId: 'aP26',
+              artistName: 'Masew',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId2',
+        content: {
+          title: 'Câu Hứa Chưa Vẹn Tròn',
+          artistName: [
+            {
+              aPId: 'aP27',
+              artistName: 'Phát',
+            },
+            {
+              aPId: 'aP28',
+              artistName: 'Huy T4',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId3',
+        content: {
+          title: 'Cưới Thôi',
+          artistName: [
+            {
+              aPId: 'aP26',
+              artistName: 'Masew',
+            },
+            {
+              aPId: 'aP29',
+              artistName: 'B Ray',
+            },
+            {
+              aPId: 'aP30',
+              artistName: 'TAP',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId4',
+        content: {
+          title: 'Thê Lương',
+          artistName: [
+            {
+              aPId: 'aP31',
+              artistName: 'Phúc Chinh',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId5',
+        content: {
+          title: 'Tình Thương Phu Thê',
+          artistName: [
+            {
+              aPId: 'aP32',
+              artistName: 'Chí Hướng',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId6',
+        content: {
+          title: 'Diệu dàng em đến',
+          artistName: [
+            {
+              aPId: 'aP33',
+              artistName: 'ERIK',
+            },
+            {
+              aPId: 'aP34',
+              artistName: 'NinjaZ',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId7',
+        content: {
+          title: 'Sài Gòn đau lòng quá',
+          artist: [
+            {
+              aPId: 'aP35',
+              artistName: 'Đinh Dũng',
+            },
+            {
+              aPId: 'aP36',
+              artistName: 'ACV',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId8',
+        content: {
+          title: 'Câu Hẹn Câu Thề',
+          artist: [
+            {
+              aPId: 'aP20',
+              artistName: 'Hứa Kim Tuyền',
+            },
+            {
+              aPId: 'aP21',
+              artistName: 'Hoàng Duyên',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId9',
+        content: {
+          title: 'Có Hẹn Với Thanh Xuân',
+          artist: [
+            {
+              aPId: 'aP37',
+              artistName: 'MONSTAR',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId10',
+        content: {
+          title: 'Muộn Rồi Mà Sao Còn',
+          artist: [
+            {
+              aPId: 'aP38',
+              artistName: 'Sơn Tùng M-TP',
+            },
+          ],
+        },
+      },
+    ],
+  },
+  {
+    sRP: 'sRP3',
+    topList: [
+      {
+        id: 'sId1',
+        avatar: {
+          avatarImage: 'ádf',
+          avatarDescription: 'da',
+        },
+        content: {
+          title: 'Ái nộ',
+          artist: [
+            {
+              aPId: 'aP26',
+              artistName: 'Masew',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId2',
+        content: {
+          title: 'Câu Hứa Chưa Vẹn Tròn',
+          artistName: [
+            {
+              aPId: 'aP27',
+              artistName: 'Phát',
+            },
+            {
+              aPId: 'aP28',
+              artistName: 'Huy T4',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId3',
+        content: {
+          title: 'Cưới Thôi',
+          artistName: [
+            {
+              aPId: 'aP26',
+              artistName: 'Masew',
+            },
+            {
+              aPId: 'aP29',
+              artistName: 'B Ray',
+            },
+            {
+              aPId: 'aP30',
+              artistName: 'TAP',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId4',
+        content: {
+          title: 'Thê Lương',
+          artistName: [
+            {
+              aPId: 'aP31',
+              artistName: 'Phúc Chinh',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId5',
+        content: {
+          title: 'Tình Thương Phu Thê',
+          artistName: [
+            {
+              aPId: 'aP32',
+              artistName: 'Chí Hướng',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId6',
+        content: {
+          title: 'Diệu dàng em đến',
+          artistName: [
+            {
+              aPId: 'aP33',
+              artistName: 'ERIK',
+            },
+            {
+              aPId: 'aP34',
+              artistName: 'NinjaZ',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId7',
+        content: {
+          title: 'Sài Gòn đau lòng quá',
+          artist: [
+            {
+              aPId: 'aP35',
+              artistName: 'Đinh Dũng',
+            },
+            {
+              aPId: 'aP36',
+              artistName: 'ACV',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId8',
+        content: {
+          title: 'Câu Hẹn Câu Thề',
+          artist: [
+            {
+              aPId: 'aP20',
+              artistName: 'Hứa Kim Tuyền',
+            },
+            {
+              aPId: 'aP21',
+              artistName: 'Hoàng Duyên',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId9',
+        content: {
+          title: 'Có Hẹn Với Thanh Xuân',
+          artist: [
+            {
+              aPId: 'aP37',
+              artistName: 'MONSTAR',
+            },
+          ],
+        },
+      },
+      {
+        id: 'sId10',
+        content: {
+          title: 'Muộn Rồi Mà Sao Còn',
+          artist: [
+            {
+              aPId: 'aP38',
+              artistName: 'Sơn Tùng M-TP',
+            },
+          ],
+        },
+      },
+    ],
+  },
+];
+
+export const songRankingPlaces = [
+  {
+    listPlace: 'Việt Nam',
+    sRP: 'sRP1',
+  },
+  {
+    listPlace: 'Âu Mỹ',
+    sRP: 'sRP2',
+  },
+  {
+    listPlace: 'Hàn Quốc',
+    sRP: 'sRP1',
+  },
+];
