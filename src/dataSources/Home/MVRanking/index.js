bcdbf03d6e750f117acbd355f4d7538c99c434c2
Thanh Minh Ho Dac <hodacthanhminh@gmail.com>
2021-11-18 17:39:23 +0700

    create dataSources

---
+++ src/dataSources/Home/MVRanking/index.js
@@ -0,0 +1,302 @@
+import mvRI1 from '../../../assets/MVRanking/1.jpg';
+import mvRI2 from '../../../assets/MVRanking/2.jpg';
+import mvRI3 from '../../../assets/MVRanking/3.jpg';
+import mvRI4 from '../../../assets/MVRanking/4.jpg';
+import mvRI5 from '../../../assets/MVRanking/5.jpg';
+
+export const mvRankingList = [
+  {
+    sRP: 'sRP1',
+    topList: [
+      {
+        id: 'mvId1',
+        avatar: {
+          avatarImage: mvRI1,
+          avatarDescription: 'Răng Khôn',
+        },
+        content: {
+          title: 'Răng Khôn',
+          artist: [
+            {
+              aPId: 'aP33',
+              artistName: 'Phí Phương Anh',
+            },
+            {
+              aPId: 'aP34',
+              artistName: 'RIN9',
+            },
+          ],
+        },
+      },
+      {
+        id: 'mvId2',
+        avatar: {
+          avatarImage: mvRI2,
+          avatarDescription: 'Dỗi vậy luôn',
+        },
+        content: {
+          title: 'Dỗi vậy luôn',
+          artist: [
+            {
+              aPId: 'aP35',
+              artistName: 'WONI',
+            },
+          ],
+        },
+      },
+      {
+        id: 'mvId3',
+        avatar: {
+          avatarImage: mvRI3,
+          avatarDescription: 'Nằm ngủ emru',
+        },
+        content: {
+          title: 'Nằm ngủ emru',
+          artist: [
+            {
+              aPId: 'aP1',
+              artistName: 'Bích Phương',
+            },
+          ],
+        },
+      },
+      {
+        id: 'mvId4',
+        avatar: {
+          avatarImage: mvRI4,
+          avatarDescription: 'Mê Chữ Ê Ê Ê',
+        },
+        content: {
+          title: 'Mê Chữ Ê Ê Ê',
+          artist: [
+            {
+              aPId: 'aP36',
+              artistName: 'Jena',
+            },
+            {
+              aPId: 'aP37',
+              artistName: '24D.Bofie',
+            },
+          ],
+        },
+      },
+      {
+        id: 'mvId5',
+        avatar: {
+          avatarImage: mvRI5,
+          avatarDescription: 'Em Ở Đâu',
+        },
+        content: {
+          title: 'Em Ở Đâu',
+          artist: [
+            {
+              aPId: 'aP39',
+              artistName: 'MANBO',
+            },
+          ],
+        },
+      },
+    ],
+  },
+  {
+    sRP: 'sRP2',
+    topList: [
+      {
+        id: 'mvId1',
+        avatar: {
+          avatarImage: mvRI1,
+          avatarDescription: 'Răng Khôn',
+        },
+        content: {
+          title: 'Răng Khôn',
+          artist: [
+            {
+              aPId: 'aP33',
+              artistName: 'Phí Phương Anh',
+            },
+            {
+              aPId: 'aP34',
+              artistName: 'RIN9',
+            },
+          ],
+        },
+      },
+      {
+        id: 'mvId2',
+        avatar: {
+          avatarImage: mvRI2,
+          avatarDescription: 'Dỗi vậy luôn',
+        },
+        content: {
+          title: 'Dỗi vậy luôn',
+          artist: [
+            {
+              aPId: 'aP35',
+              artistName: 'WONI',
+            },
+          ],
+        },
+      },
+      {
+        id: 'mvId3',
+        avatar: {
+          avatarImage: mvRI3,
+          avatarDescription: 'Nằm ngủ emru',
+        },
+        content: {
+          title: 'Nằm ngủ emru',
+          artist: [
+            {
+              aPId: 'aP1',
+              artistName: 'Bích Phương',
+            },
+          ],
+        },
+      },
+      {
+        id: 'mvId4',
+        avatar: {
+          avatarImage: mvRI4,
+          avatarDescription: 'Mê Chữ Ê Ê Ê',
+        },
+        content: {
+          title: 'Mê Chữ Ê Ê Ê',
+          artist: [
+            {
+              aPId: 'aP36',
+              artistName: 'Jena',
+            },
+            {
+              aPId: 'aP37',
+              artistName: '24D.Bofie',
+            },
+          ],
+        },
+      },
+      {
+        id: 'mvId5',
+        avatar: {
+          avatarImage: mvRI5,
+          avatarDescription: 'Em Ở Đâu',
+        },
+        content: {
+          title: 'Em Ở Đâu',
+          artist: [
+            {
+              aPId: 'aP39',
+              artistName: 'MANBO',
+            },
+          ],
+        },
+      },
+    ],
+  },
+  {
+    sRP: 'sRP3',
+    topList: [
+      {
+        id: 'mvId1',
+        avatar: {
+          avatarImage: mvRI1,
+          avatarDescription: 'Răng Khôn',
+        },
+        content: {
+          title: 'Răng Khôn',
+          artist: [
+            {
+              aPId: 'aP33',
+              artistName: 'Phí Phương Anh',
+            },
+            {
+              aPId: 'aP34',
+              artistName: 'RIN9',
+            },
+          ],
+        },
+      },
+      {
+        id: 'mvId2',
+        avatar: {
+          avatarImage: mvRI2,
+          avatarDescription: 'Dỗi vậy luôn',
+        },
+        content: {
+          title: 'Dỗi vậy luôn',
+          artist: [
+            {
+              aPId: 'aP35',
+              artistName: 'WONI',
+            },
+          ],
+        },
+      },
+      {
+        id: 'mvId3',
+        avatar: {
+          avatarImage: mvRI3,
+          avatarDescription: 'Nằm ngủ emru',
+        },
+        content: {
+          title: 'Nằm ngủ emru',
+          artist: [
+            {
+              aPId: 'aP1',
+              artistName: 'Bích Phương',
+            },
+          ],
+        },
+      },
+      {
+        id: 'mvId4',
+        avatar: {
+          avatarImage: mvRI4,
+          avatarDescription: 'Mê Chữ Ê Ê Ê',
+        },
+        content: {
+          title: 'Mê Chữ Ê Ê Ê',
+          artist: [
+            {
+              aPId: 'aP36',
+              artistName: 'Jena',
+            },
+            {
+              aPId: 'aP37',
+              artistName: '24D.Bofie',
+            },
+          ],
+        },
+      },
+      {
+        id: 'mvId5',
+        avatar: {
+          avatarImage: mvRI5,
+          avatarDescription: 'Em Ở Đâu',
+        },
+        content: {
+          title: 'Em Ở Đâu',
+          artist: [
+            {
+              aPId: 'aP39',
+              artistName: 'MANBO',
+            },
+          ],
+        },
+      },
+    ],
+  },
+];
+
+export const mvRankingPlaces = [
+  {
+    listPlace: 'Việt Nam',
+    sRP: 'sRP1',
+  },
+  {
+    listPlace: 'Âu Mỹ',
+    sRP: 'sRP2',
+  },
+  {
+    listPlace: 'Hàn Quốc',
+    sRP: 'sRP1',
+  },
+];
