90bc88a22e9b32e9fb809aea6b9f776c728462ec
Thanh Minh Ho Dac <hodacthanhminh@gmail.com>
2021-11-18 10:24:00 +0700

    edit card detail

--- src/Home/components/Card/components/CardDetail/index.jsx
+++ src/Home/components/Card/components/CardDetail/index.jsx
@@ -4,28 +4,25 @@ import PropTypes from 'prop-types';
 // style
 import './style.scss';
 // components
-import { CustomerServiceOutlined } from '@ant-design/icons';
+import { CustomerServiceOutlined, EyeFilled } from '@ant-design/icons';
 
 const CardDetail = ({ cardDetail, cardType }) => (
   <div className={['card-detail-wrapper', `card-detail-${cardType}`].join(' ')}>
     <div className="card-detail-wrapper-inner">
       {cardDetail.listenAmount && (
-        <div className="card-detail-listen">
+        <div className="card-detail detail-listen">
           <CustomerServiceOutlined />
           {cardDetail.listenAmount}
         </div>
       )}
       {cardDetail.watchAmount && (
-        <div className="card-detail-watch">
-          <CustomerServiceOutlined />
+        <div className="card-detail detail-watch">
+          <EyeFilled />
           {cardDetail.watchAmount}
         </div>
       )}
       {cardDetail.videoTime && (
-        <div className="card-detail-time">
-          <CustomerServiceOutlined />
-          {cardDetail.videoTime}
-        </div>
+        <div className="card-detail detail-time">{cardDetail.videoTime}</div>
       )}
     </div>
   </div>
