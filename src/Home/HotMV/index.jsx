8609c74a3a7f623e014945aa5dc4c850abebef7f
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-11-18 08:47:58 +0700

    create HotMVBody and view

--- src/Home/HotMV/index.jsx
+++ src/Home/HotMV/index.jsx
@@ -1,9 +1,20 @@
 // lib
 import React from 'react';
-
 // style
 import './style.scss';
+// components
+import Heading from '../components/Heading';
+import HotMVBody from './mains/HotMVBody';
+// other
+import { hotMVList } from '../../dataSources/Home/HotMV';
 
-const HotMV = () => <div className="hot-mv-wrapper">Hot MV</div>;
+const HotMV = () => (
+  <div className="hot-mv-wrapper">
+    <div className="hot-mv-wrapper-inner">
+      <Heading title="HOT MV" icon />
+      <HotMVBody mvList={hotMVList} />
+    </div>
+  </div>
+);
 
 export default HotMV;
