537e4ed8e62dc79225c8cf489a58f9bdaf770843
Thanh Minh Ho Dac <hodacthanhminh@gmail.com>
2021-11-19 10:42:13 +0700

    create footer information certificate

--- src/Home/Footer/mains/CompanyInformation/index.jsx
+++ src/Home/Footer/mains/CompanyInformation/index.jsx
@@ -1,12 +1,14 @@
 // libs
 import React from 'react';
+// components
+import CompanyCertification from './mains/CompanyCertification';
 // style
 import './style.scss';
 
 const CompanyInformation = () => (
   <div className="company-information-wrapper">
     <div className="company-information-wrapper-inner">
-      THis is company information
+      <CompanyCertification />
     </div>
   </div>
 );
