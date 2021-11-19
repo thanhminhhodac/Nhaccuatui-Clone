8d6069bcaa92024c96ee7b86fe2630d9c637a63b
Thanh Minh Ho Dac <hodacthanhminh@gmail.com>
2021-11-19 08:25:11 +0700

    footer contact component

--- src/Home/Footer/mains/CompanyContact/index.jsx
+++ src/Home/Footer/mains/CompanyContact/index.jsx
@@ -1,11 +1,27 @@
 // libs
 import React from 'react';
+// components
+import CompanyContactInfo from '../../components/CompanyContactInfo';
 // style
 import './style.scss';
 
+const contactData = {
+  companyLocation:
+    'Tầng 19, Tòa nhà The 67B Tower, Số 67 đường Hoàng Văn Thái, phường Tân Phú, quận 7, TP HCM.',
+  companyMail: 'support@nct.vn',
+  companyPhone: '(028) 3868 7979',
+};
+
 const CompanyContact = () => (
   <div className="company-contact-wrapper">
-    <div className="company-contact-wrapper-inner">THis is company contact</div>
+    <div className="company-contact-wrapper-inner">
+      <CompanyContactInfo contact={contactData} />
+      <div className="company-contact-copyrighted">
+        <span className="company-contact-copyright-label">
+          © NCT Corp.All rights reserved.
+        </span>
+      </div>
+    </div>
   </div>
 );
 
