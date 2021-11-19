4da934c58069841485e69c7266d80447b323655b
Thanh Minh Ho Dac <hodacthanhminh@gmail.com>
2021-11-19 15:09:04 +0700

    Create dataSources

---
+++ src/dataSources/Home/Footer/companyInformation/index.js
@@ -0,0 +1,76 @@
+import sMI1 from '../../../../assets/Footer/t_ic_facebook.png';
+import sMI2 from '../../../../assets/Footer/t_ic_instagram.png';
+import sMI3 from '../../../../assets/Footer/t_ic_tiktok.png';
+import sMI4 from '../../../../assets/Footer/t_ic_zalo.png';
+import aI1 from '../../../../assets/Footer/t_google_play.png';
+import aI2 from '../../../../assets/Footer/t_app_store.png';
+import aI3 from '../../../../assets/Footer/t_appgallery.png';
+
+export const supportlabels = [
+  'Trợ giúp',
+  'Chính sách bảo mật',
+  'Sơ đồ',
+  'Chính sách SHTT',
+  'NCCI',
+  'Thỏa thuận sử dụng',
+  'Liên hệ quảng cáo',
+];
+
+export const moreServicelabels = [
+  'Moblie App',
+  'Dịch vụ 3G',
+  'Moblie Web',
+  'NCT Corp',
+  'Smart TV',
+  'Tuyển dụng',
+  'Desktop',
+];
+
+export const keyWords = [
+  'Muộn Rồi Mà Sao Còn',
+  'Sài Gòn Đau Lòng Quá',
+  'Thưởng thức nỗi buồn',
+  'Laylalay',
+  'Một Cái Tên',
+];
+
+export const socialMedias = [
+  {
+    id: 'sM1',
+    sMImage: sMI1,
+    sMDescription: 'facebook',
+  },
+  {
+    id: 'sM2',
+    sMImage: sMI2,
+    sMDescription: 'instagram',
+  },
+  {
+    id: 'sM3',
+    sMImage: sMI3,
+    sMDescription: 'Tiktok',
+  },
+  {
+    id: 'sM4',
+    sMImage: sMI4,
+    sMDescription: 'zalo',
+  },
+];
+
+export const apps = [
+  {
+    id: 'a1',
+    aImage: aI1,
+    aDescription: 'appGooglePlay',
+  },
+  {
+    id: 'a2',
+    aImage: aI2,
+    aDescription: 'appStore',
+  },
+  {
+    id: 'a3',
+    aImage: aI3,
+    aDescription: 'appGalalary',
+  },
+];
