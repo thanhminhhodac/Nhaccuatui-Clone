37a3a4cde568ebd97c4aeaeb6cfbcc095260e5da
Thanh Minh Ho Dac <hodacthanhminh@gmail.com>
2021-11-22 16:10:01 +0700

    Move context to AuthenticationItem

--- src/Home/Header/Authentication/index.jsx
+++ src/Home/Header/Authentication/index.jsx
@@ -6,33 +6,25 @@ import AuthenticationItem from './mains/AuthenticationItem';
 // style
 import './style.scss';
 // context
-import { useLocale } from '../../../contexts/LocaleContext';
-// other
 
-const Authentication = ({ authenticationList }) => {
-  const { localeDataSource } = useLocale();
-  const { authenticationName } = localeDataSource;
-  console.log(authenticationName);
-  return (
-    <div className="authentication-wrapper">
-      <div className="authentication-wrapper-inner">
-        {authenticationList.map((authentication) => (
-          <AuthenticationItem
-            className={authentication.className}
-            label={authenticationName[authentication.name]}
-            key={authentication.id}
-          />
-        ))}
-      </div>
+const Authentication = ({ authenticationList }) => (
+  <div className="authentication-wrapper">
+    <div className="authentication-wrapper-inner">
+      {authenticationList.map((authentication) => (
+        <AuthenticationItem
+          className={authentication.className}
+          name={authentication.name}
+          key={authentication.id}
+        />
+      ))}
     </div>
-  );
-};
+  </div>
+);
 
 Authentication.propTypes = {
   authenticationList: PropTypes.arrayOf(
     PropTypes.shape({
       className: PropTypes.string,
-      label: PropTypes.string,
       name: PropTypes.string,
       id: PropTypes.string,
     }),
