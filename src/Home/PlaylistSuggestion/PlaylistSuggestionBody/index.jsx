06145de3e1c4bae536d13271984714bffddf0409
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-11-25 10:40:59 +0700

    using antd pagination and event handle in PlaylistSuggestionBody

--- src/Home/PlaylistSuggestion/PlaylistSuggestionBody/index.jsx
+++ src/Home/PlaylistSuggestion/PlaylistSuggestionBody/index.jsx
@@ -1,22 +1,49 @@
 // libs
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Pagination } from 'antd';
 // style
 import './style.scss';
 // components
 import Card from '../../components/Card';
 
-const PlaylistSuggestionBody = ({ suggestList }) => (
-  <div className="playlist-suggestion-body">
-    {suggestList.map((suggest) => (
-      <Card
-        cardItem={suggest}
-        cardType="playlist-suggestion"
-        key={suggest.id}
+const PlaylistSuggestionBody = ({ suggestList }) => {
+  const [currentPage, setcurrentPage] = useState(1);
+  const [listShow, setListShow] = useState([]);
+  const [itemPerPage] = useState(5);
+
+  const handleChangePage = (page) => setcurrentPage(page);
+
+  useEffect(() => {
+    const newList = suggestList.filter(
+      (item, index) =>
+        index >= (currentPage - 1) * itemPerPage &&
+        index < currentPage * itemPerPage,
+    );
+    setListShow(newList);
+  }, [currentPage]);
+
+  return (
+    <div className="playlist-suggestion-body">
+      <div className="playlist-suggestion-body-card">
+        {listShow.map((suggest) => (
+          <Card
+            cardItem={suggest}
+            cardType="playlist-suggestion"
+            key={suggest.id}
+          />
+        ))}
+      </div>
+      <Pagination
+        className="playlist-suggestion-pagination"
+        current={currentPage}
+        onChange={handleChangePage}
+        total={suggestList.length}
+        defaultPageSize={itemPerPage}
       />
-    ))}
-  </div>
-);
+    </div>
+  );
+};
 
 PlaylistSuggestionBody.propTypes = {
   suggestList: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
--- src/dataSources/Home/PlaylistSuggestion/index.js
+++ src/dataSources/Home/PlaylistSuggestion/index.js
@@ -69,6 +69,106 @@ export const suggestList = [
           title: 'Độc thân vui vẻ',
         },
       },
+      {
+        id: 'sPI6',
+        avatar: {
+          avatarImage: sPL6,
+          avatarDescription: 'Giai điệu Bắt Tai US - UK',
+        },
+        content: {
+          title: 'Giai điệu Bắt Tai US - UK',
+        },
+      },
+      {
+        id: 'sPI7',
+        avatar: {
+          avatarImage: sPL7,
+          avatarDescription: 'V-POP Những bản hit Quốc Dân',
+        },
+        content: {
+          title: 'V-POP Những bản hit Quốc Dân',
+        },
+      },
+      {
+        id: 'sPI8',
+        avatar: {
+          avatarImage: sPL8,
+          avatarDescription: 'Nhạc mới tuần này',
+        },
+        content: {
+          title: 'Nhạc mới tuần này',
+        },
+      },
+      {
+        id: 'sPI9',
+        avatar: {
+          avatarImage: sPL9,
+          avatarDescription: 'Ballad Việt không thể bỏ qua',
+        },
+        content: {
+          title: 'Ballad Việt không thể bỏ qua',
+        },
+      },
+      {
+        id: 'sPI10',
+        avatar: {
+          avatarImage: sPL10,
+          avatarDescription: 'Future K-POP 2021',
+        },
+        content: {
+          title: 'Future K-POP 2021',
+        },
+      },
+      {
+        id: 'sPI11',
+        avatar: {
+          avatarImage: sPL11,
+          avatarDescription: 'Chơi cho hết đời tuổi trẻ',
+        },
+        content: {
+          title: 'Chơi cho hết đời tuổi trẻ',
+        },
+      },
+      {
+        id: 'sPI12',
+        avatar: {
+          avatarImage: sPL12,
+          avatarDescription: 'Vitamin Sea',
+        },
+        content: {
+          title: 'Vitamin Sea',
+        },
+      },
+      {
+        id: 'sPI13',
+        avatar: {
+          avatarImage: sPL13,
+          avatarDescription: 'Bên em là anh',
+        },
+        content: {
+          title: 'Bên em là anh',
+        },
+      },
+      {
+        id: 'sPI14',
+        avatar: {
+          avatarImage: sPL14,
+          avatarDescription: 'Mình đi cùng nhau',
+        },
+        content: {
+          title: 'Mình đi cùng nhau',
+        },
+      },
+      {
+        id: 'sPI15',
+        avatar: {
+          avatarImage: sPL15,
+          avatarDescription: 'Bạn thân ơi, mình đi thôi',
+        },
+        content: {
+          title: 'Bạn thân ơi, mình đi thôi',
+        },
+      },
     ],
   },
   {
