68d9909b36ba92a063fdbec5658c189fdcd98c4b
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-11-26 10:49:40 +0700

    update playlistsuggestion dataSource

--- src/dataSources/Home/PlaylistSuggestion/index.js
+++ src/dataSources/Home/PlaylistSuggestion/index.js
@@ -13,11 +13,29 @@ import sPL12 from '../../../assets/playlistsuggestion/12.jpg';
 import sPL13 from '../../../assets/playlistsuggestion/13.jpg';
 import sPL14 from '../../../assets/playlistsuggestion/14.jpg';
 import sPL15 from '../../../assets/playlistsuggestion/15.jpg';
+import sPL16 from '../../../assets/playlistsuggestion/16.jpg';
+import sPL17 from '../../../assets/playlistsuggestion/17.jpg';
+import sPL18 from '../../../assets/playlistsuggestion/18.jpg';
+import sPL19 from '../../../assets/playlistsuggestion/19.jpg';
+import sPL20 from '../../../assets/playlistsuggestion/20.jpg';
+import sPL21 from '../../../assets/playlistsuggestion/21.jpg';
+import sPL22 from '../../../assets/playlistsuggestion/22.jpg';
+import sPL23 from '../../../assets/playlistsuggestion/23.jpg';
+import sPL24 from '../../../assets/playlistsuggestion/24.jpg';
+import sPL25 from '../../../assets/playlistsuggestion/25.jpg';
+import sPL26 from '../../../assets/playlistsuggestion/26.jpg';
+import sPL27 from '../../../assets/playlistsuggestion/27.jpg';
+import sPL28 from '../../../assets/playlistsuggestion/28.jpg';
+import sPL29 from '../../../assets/playlistsuggestion/29.jpg';
+import sPL30 from '../../../assets/playlistsuggestion/30.jpg';
+import sPL31 from '../../../assets/playlistsuggestion/31.jpg';
+import sPL32 from '../../../assets/playlistsuggestion/32.jpg';
+import sPL33 from '../../../assets/playlistsuggestion/33.jpg';
 
 export const suggestList = [
   {
     sPLid: 'sPL1',
-    title: 'Nếu bạn độc thân',
+    title: 'Dành riêng cho bạn',
     list: [
       {
         id: 'sPI1',
@@ -43,10 +61,10 @@ export const suggestList = [
         id: 'sPI3',
         avatar: {
           avatarImage: sPL3,
-          avatarDescription: "I'm sPingle",
+          avatarDescription: "I'm Single",
         },
         content: {
-          title: "I'm sPingle",
+          title: "I'm Single",
         },
       },
       {
@@ -70,13 +88,13 @@ export const suggestList = [
         },
       },
       {
-        id: 'sPI6',
+        id: 'sPI25',
         avatar: {
-          avatarImage: sPL6,
-          avatarDescription: 'Giai điệu Bắt Tai US - UK',
+          avatarImage: sPL25,
+          avatarDescription: 'Lofi Beat Cafe',
         },
         content: {
-          title: 'Giai điệu Bắt Tai US - UK',
+          title: 'Lofi Beat Cafe',
         },
       },
       {
@@ -176,109 +194,189 @@ export const suggestList = [
     title: 'Khám phá mới',
     list: [
       {
+        id: 'sPI16',
+        avatar: {
+          avatarImage: sPL16,
+          avatarDescription: 'TOP Trending TIKTOK Songs 2021',
+        },
+        content: {
+          title: 'TOP Trending TIKTOK Songs 2021',
+        },
+      },
+      {
+        id: 'sPI17',
+        avatar: {
+          avatarImage: sPL17,
+          avatarDescription: 'POP TOP',
+        },
+        content: {
+          title: 'POP TOP',
+        },
+      },
+      {
+        id: 'sPI18',
+        avatar: {
+          avatarImage: sPL18,
+          avatarDescription: 'New US-UK Generation',
+        },
+        content: {
+          title: 'New US-UK Generation',
+        },
+      },
+      {
+        id: 'sPI19',
+        avatar: {
+          avatarImage: sPL19,
+          avatarDescription: 'US-UK Không Thể Bỏ Qua',
+        },
+        content: {
+          title: 'US-UK Không Thể Bỏ Qua',
+        },
+      },
+      {
+        id: 'sPI20',
+        avatar: {
+          avatarImage: sPL20,
+          avatarDescription: 'Người Chơi Nhạc Âu Mỹ',
+        },
+        content: {
+          title: 'Người Chơi Nhạc Âu Mỹ',
+        },
+      },
+      {
+        id: 'sPI21',
+        avatar: {
+          avatarImage: sPL21,
+          avatarDescription: 'Dance Pop Hits',
+        },
+        content: {
+          title: 'Dance Pop Hits',
+        },
+      },
+      {
+        id: 'sPI22',
+        avatar: {
+          avatarImage: sPL22,
+          avatarDescription: 'Top 20 US-UK',
+        },
+        content: {
+          title: 'Top 20 US-UK',
+        },
+      },
+      {
+        id: 'sPI23',
+        avatar: {
+          avatarImage: sPL23,
+          avatarDescription: 'US-UK Đỉnh Cao Collab',
+        },
+        content: {
+          title: 'US-UK Đỉnh Cao Collab',
+        },
+      },
+      {
+        id: 'sPI24',
+        avatar: {
+          avatarImage: sPL24,
+          avatarDescription: 'Giai Điệu Bắt Tai US-UK',
+        },
+        content: {
+          title: 'Giai Điệu Bắt Tai US-UK',
+        },
+      },
+      {
         id: 'sPI6',
         avatar: {
           avatarImage: sPL6,
-          avatarDescription: 'Giai điệu Bắt Tai US - UK',
+          avatarDescription: 'Sing Your Heart Out',
         },
         content: {
-          title: 'Giai điệu Bắt Tai US - UK',
-        },
-      },
-      {
-        id: 'sPI7',
-        avatar: {
-          avatarImage: sPL7,
-          avatarDescription: 'V-POP Những bản hit Quốc Dân',
-        },
-        content: {
-          title: 'V-POP Những bản hit Quốc Dân',
-        },
-      },
-      {
-        id: 'sPI8',
-        avatar: {
-          avatarImage: sPL8,
-          avatarDescription: 'Nhạc mới tuần này',
-        },
-        content: {
-          title: 'Nhạc mới tuần này',
-        },
-      },
-      {
-        id: 'sPI9',
-        avatar: {
-          avatarImage: sPL9,
-          avatarDescription: 'Ballad Việt không thể bỏ qua',
-        },
-        content: {
-          title: 'Ballad Việt không thể bỏ qua',
-        },
-      },
-      {
-        id: 'sPI10',
-        avatar: {
-          avatarImage: sPL10,
-          avatarDescription: 'Future K-POP 2021',
-        },
-        content: {
-          title: 'Future K-POP 2021',
+          title: 'Sing Your Heart Out',
         },
       },
     ],
   },
   {
     sPLid: 'sPL3',
-    title: 'Hết giãn cách mình sẽ',
+    title: 'Luyện tập cùng tôi',
     list: [
       {
-        id: 'sPI11',
+        id: 'sPI27',
         avatar: {
-          avatarImage: sPL11,
-          avatarDescription: 'Chơi cho hết đời tuổi trẻ',
+          avatarImage: sPL27,
+          avatarDescription: 'Home Workout',
         },
         content: {
-          title: 'Chơi cho hết đời tuổi trẻ',
+          title: 'Home Workout',
         },
       },
       {
-        id: 'sPI12',
+        id: 'sPI28',
         avatar: {
-          avatarImage: sPL12,
-          avatarDescription: 'Vitamin Sea',
+          avatarImage: sPL28,
+          avatarDescription: 'Trở Lại Mạnh Mẽ',
         },
         content: {
-          title: 'Vitamin Sea',
+          title: 'Trở Lại Mạnh Mẽ',
         },
       },
       {
-        id: 'sPI13',
+        id: 'sPI29',
         avatar: {
-          avatarImage: sPL13,
-          avatarDescription: 'Bên em là anh',
+          avatarImage: sPL29,
+          avatarDescription: 'Warrior Workout',
         },
         content: {
-          title: 'Bên em là anh',
+          title: 'Warrior Workout',
         },
       },
       {
-        id: 'sPI14',
+        id: 'sPI31',
         avatar: {
-          avatarImage: sPL14,
-          avatarDescription: 'Mình đi cùng nhau',
+          avatarImage: sPL31,
+          avatarDescription: 'Workout From Home',
         },
         content: {
-          title: 'Mình đi cùng nhau',
+          title: 'Workout From Home',
         },
       },
       {
-        id: 'sPI15',
+        id: 'sPI32',
         avatar: {
-          avatarImage: sPL15,
-          avatarDescription: 'Bạn thân ơi, mình đi thôi',
+          avatarImage: sPL32,
+          avatarDescription: 'Push The Limits',
         },
         content: {
-          title: 'Bạn thân ơi, mình đi thôi',
+          title: 'Push The Limits',
+        },
+      },
+      {
+        id: 'sPI33',
+        avatar: {
+          avatarImage: sPL33,
+          avatarDescription: 'BurningUP',
+        },
+        content: {
+          title: 'BurningUP',
+        },
+      },
+      {
+        id: 'sPI26',
+        avatar: {
+          avatarImage: sPL26,
+          avatarDescription: 'Tập Hăng Say',
+        },
+        content: {
+          title: 'Tập Hăng Say',
+        },
+      },
+      {
+        id: 'sPI30',
+        avatar: {
+          avatarImage: sPL30,
+          avatarDescription: 'Tập Thể Dục Nâng Cao',
+        },
+        content: {
+          title: 'Tập Thể Dục Nâng Cao',
         },
       },
     ],
