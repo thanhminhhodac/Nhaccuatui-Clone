4e6442c4037b496c9dd520147eb717dc0a3352fd
thanhminhhodac <thanhminh.hodac@m.citynow.vn>
2021-11-28 22:46:46 +0700

    using new custom hook

--- src/Home/PlaylistSuggestion/PlaylistSuggestionBody/index.jsx
+++ src/Home/PlaylistSuggestion/PlaylistSuggestionBody/index.jsx
@@ -1,5 +1,5 @@
 // libs
-import React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { Pagination } from 'antd';
 // style
@@ -7,56 +7,22 @@ import './style.scss';
 // components
 import Card from '../../components/Card';
 // hooks
-import { useEventListener } from '../../../hooks/useEventListener';
+import { usePagination } from '../../../hooks/usePagination';
 
 const PlaylistSuggestionBody = ({ suggestList }) => {
-  const [currentPage, setcurrentPage] = useState(1);
   const [listShow, setListShow] = useState([]);
-  const [itemPerPage] = useState(5);
-  const [hover, setHover] = useState(false);
-  const ref = useRef(null);
+  const [currentPage, beginItem, endItem, setOnChangePage, ref] = usePagination(
+    {
+      currentPage: 1,
+      itemsPerPage: 5,
+      totalItem: suggestList.length,
+    },
+  );
 
   useEffect(() => {
-    const newList = suggestList.filter(
-      (item, index) =>
-        index >= (currentPage - 1) * itemPerPage &&
-        index < currentPage * itemPerPage,
-    );
+    const newList = suggestList.slice(beginItem, endItem);
     setListShow(newList);
   }, [currentPage]);
-
-  const handleOnChangePage = (page) => setcurrentPage(page);
-  const handleMouseOver = () => {
-    setHover(true);
-  };
-  const handleMouseOut = () => {
-    setHover(false);
-  };
-  const handleKeyDown = (e) => {
-    if (hover) {
-      e.preventDefault();
-      switch (e.key) {
-        case 'PageUp':
-          {
-            const nextpage = currentPage + 1;
-            if (nextpage * itemPerPage > suggestList.length) break;
-          }
-          return setcurrentPage(currentPage + 1);
-        case 'PageDown':
-          {
-            const nextpage = currentPage - 1;
-            if (nextpage < 1) break;
-          }
-          return setcurrentPage(currentPage - 1);
-        default:
-          return setcurrentPage(currentPage);
-      }
-    }
-  };
-
-  useEventListener('keydown', handleKeyDown);
-  useEventListener('mouseover', handleMouseOver, ref.current);
-  useEventListener('mouseout', handleMouseOut, ref.current);
 
   return (
     <div className="playlist-suggestion-body" ref={ref}>
@@ -72,9 +38,9 @@ const PlaylistSuggestionBody = ({ suggestList }) => {
       <Pagination
         className="playlist-suggestion-pagination"
         current={currentPage}
-        onChange={handleOnChangePage}
+        onChange={setOnChangePage}
         total={suggestList.length}
-        defaultPageSize={itemPerPage}
+        defaultPageSize={5}
       />
     </div>
   );
